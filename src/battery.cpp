f888b8edc6092f2b2c7173214401f1804755d09e
benboydens <r0857165@student.vives.be>
2021-10-08 08:42:14 +0200

    boiler plate

---
+++ src/battery.cpp
@@ -0,0 +1,9 @@
+#include "battery.h"
+
+Battery::Battery() {
+    // constructor
+}
+
+int Battery::getPercent() {
+    // get percent
+}
---
+++ src/battery.h
@@ -0,0 +1,5 @@
+class Battery {
+    public:
+        Battery();
+        int getPercent();
+}
