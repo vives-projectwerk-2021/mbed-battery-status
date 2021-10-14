3595be0cc40b58cfbb30ff534c4447b04070e3d0
benboydens <r0857165@student.vives.be>
2021-10-14 13:52:55 +0200

    battery status in float

--- src/battery.cpp
+++ src/battery.cpp
@@ -4,6 +4,10 @@ Battery::Battery() {
     // constructor
 }
 
-int Battery::getPercent() {
-    // get percent
+float Battery::voltage() {
+    // get voltage
+    float vref = adc_vref.read();
+    float vbat = adc_vbat.read() * 3;
+
+    return V_REFINT * (vbat/vref);
 }
--- src/battery.h
+++ src/battery.h
@@ -1,5 +1,13 @@
+#pragma once
+
+#include "mbed.h"
+
+#define V_REFINT 1.212
+AnalogIn adc_vref(ADC_VREF);
+AnalogIn adc_vbat(ADC_VBAT);
+
 class Battery {
     public:
         Battery();
-        int getPercent();
+        float voltage();
 }
