0395591fb2fe62e41cd60cb4fba85eb225a378a0
benboydens <r0857165@student.vives.be>
2021-10-14 16:07:15 +0200

    update with internal calibration value

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,17 +1,20 @@
 #include "battery.h"
 
+
+// constructor
 Battery::Battery():
     adc_vref(ADC_VREF),
     adc_vbat(ADC_VBAT)
 {
-    // constructor
+    // calibratie waarde van Vrefint (address staat in datasheet)
+    v_ref_cal = *((uint16_t*)0x1FFF75AA);
 }
 
-float Battery::voltage()
-{
-    // get voltage
-    float vref = adc_vref.read();
-    float vbat = adc_vbat.read() * 3;
+// get voltage   
+float Battery::voltage() { 
+    uint16_t vref = adc_vref.read_u16();
+    uint16_t vbat = adc_vbat.read_u16() * 3;
 
-    return V_REFINT * (vbat/vref);
+    float vrefplus = V_REF_PLUS_CHAR * v_ref_cal / vref;
+    return (vrefplus * vbat) / FULL_SCALE;
 }
--- src/battery.h
+++ src/battery.h
@@ -2,12 +2,14 @@
 
 #include "mbed.h"
 
-#define V_REFINT 1.212
+#define V_REF_PLUS_CHAR 3.0
+#define FULL_SCALE 4095
 
 class Battery {
     private:
         AnalogIn adc_vref;
         AnalogIn adc_vbat;
+        uint16_t v_ref_cal;
     public:
         Battery();
         float voltage();
